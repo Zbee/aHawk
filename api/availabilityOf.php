2169b317cfa5ec462ff19036514abcae32a1e3cc
Ethan Henderson <ethan@zbee.me>
2015-07-12 10:17:43 -0600

    API expanded endpoint names and switched die to throw

--- api/availabilityOf.php
+++ api/availabilityOf.php
@@ -5,122 +5,72 @@ $url = explode('/', $url);
 
 $args = explode('?', end($url));
 if (count($args) === 1 || strtolower(explode('=', $args[1])[0]) !== 'token')
-  die(
-    json_encode(
-      [
-        'status' => [404, 'this endpoint requires a token (' . $urlO . ')']
-      ]
-    )
+  throw new Exception(
+    'this endpoint requires a token (' . $urlO . ')'
   );
 
 $token = preg_replace('/[^a-z0-9]+/i', '', explode('=', $args[1])[1]);
 if (strlen($token) !== 20)
-  die(
-    json_encode(
-      [
-        'status' => [404, 'token provided is not valid (' . $token . ')']
-      ]
-    )
+  throw new Exception(
+    'token provided is not valid (' . $token . ')'
   );
 
 $realm = preg_replace('/[^\w\'\- ]+/i', '', urldecode($url[0]));
 if ($realm == '')
-  die(
-    json_encode(
-      [
-        'status' => [404, 'realm not found in url (' . $urlO . ')']
-      ]
-    )
+  throw new Exception(
+    'realm not found in url (' . $urlO . ')'
   );
 
 $item = intval($url[1]);
 if ($item == 0)
-  die(
-    json_encode(
-      [
-        'status' => [404, 'item not found in url (' . $urlO . ')']
-      ]
-    )
+  throw new Exception(
+    'item not found in url (' . $urlO . ')'
   );
 
 $time = strtolower(preg_replace('/[^0-9a-zA-Z]+/i', '', explode('?', $url[2])[0]));
 if ($time == '')
-  die(
-    json_encode(
-      [
-        'status' => [404, 'time not found in url (' . $urlO . ')']
-      ]
-    )
+  throw new Exception(
+    'time not found in url (' . $urlO . ')'
   );
 
 $tokenSearch = $tokenExist = $controller->select('apis', ['token' => $token]);
 $tokenExist = @count($tokenExist) === 1 ? true : false;
 if (!$tokenExist)
-  die(
-    json_encode(
-      [
-        'status' => [404, 'token not found (' . $token . ')']
-      ]
-    )
+  throw new Exception(
+    'token not found (' . $token . ')'
   );
 
 $fineIPs = explode('&', explode(',', $tokenSearch[0])[3]);
 if (!in_array($_SERVER['REMOTE_ADDR'], $fineIPs) && !in_array('*', $fineIPs))
-  die(
-    json_encode(
-      [
-        'status' => [
-          404,
-          'this IP does not match this token (' . $_SERVER['REMOTE_ADDR'] . ')'
-        ]
-      ]
-    )
+  throw new Exception(
+    'this IP does not match this token (' . $_SERVER['REMOTE_ADDR'] . ')'
   );
 
 $realmExist = $controller->select('realms', ['name' => $realm]);
 $realmExist = @count($realmExist) === 1 ? true : false;
 if (!$realmExist)
-  die(
-    json_encode(
-      [
-        'status' => [404, 'realm not found (' . $realm . ')']
-      ]
-    )
+  throw new Exception(
+    'realm not found (' . $realm . ')'
   );
 
 $itemExist = file_exists('../assets/data/items/' . $item . '.dat');
 if (!$itemExist)
-  die(
-    json_encode(
-      [
-        'status' => [404, 'item not found (' . $item . ')']
-      ]
-    )
+  throw new Exception(
+    'item not found (' . $item . ')'
   );
 
 $timeAllowed = $time == 'now' || $time == 'today'  || is_numeric($time)
   ? true
   : false;
 if (!$timeAllowed)
-  die(
-    json_encode(
-      [
-        'status' => [
-          404,
-          'time only accepts "now", "today", and integers (' . $time . ')'
-        ]
-      ]
-    )
+  throw new Exception(
+    'time only accepts "now", "today", and integers (' . $time . ')'
   );
 
 $availability = $controller->availabilityOf($item, $realm, $time, 'available');
 if (!is_object($availability))
-  die(
-    json_encode(
-      [
-        'status' => [404, 'item not being tracked (' . $item . ')']
-      ]
-    )
+  throw new Exception(
+    'item not being tracked (' . $item . ')'
   );
 
 http_response_code(200);
--- api/json.php
+++ api/json.php
@@ -5,53 +5,33 @@ $url = explode('/', $url);
 
 $realm = preg_replace('/[^\w\'\- ]+/i', '', urldecode($url[0]));
 if ($realm == '')
-  die(
-    json_encode(
-      [
-        'status' => [404, 'realm not found in url (' . $urlO . ')']
-      ]
-    )
+  throw new Exception(
+    'realm not found in url (' . $urlO . ')'
   );
 
 $item = intval($url[1]);
 if ($item == 0)
-  die(
-    json_encode(
-      [
-        'status' => [404, 'item not found in url (' . $urlO . ')']
-      ]
-    )
+  throw new Exception(
+    'item not found in url (' . $urlO . ')'
   );
 
 $realmExist = $controller->select('realms', ['name' => $realm]);
 $realmExist = @count($realmExist) === 1 ? true : false;
 if (!$realmExist)
-  die(
-    json_encode(
-      [
-        'status' => [404, 'realm not found (' . $realm . ')']
-      ]
-    )
+  throw new Exception(
+    'realm not found (' . $realm . ')'
   );
 
 $itemExist = file_exists('../assets/data/items/' . $item . '.dat');
 if (!$itemExist)
-  die(
-    json_encode(
-      [
-        'status' => [404, 'item not found (' . $item . ')']
-      ]
-    )
+  throw new Exception(
+    'item not found (' . $item . ')'
   );
 
 $availability = $controller->availabilityOf($item, $realm);
 if (!is_object($availability))
-  die(
-    json_encode(
-      [
-        'status' => [404, 'item not being tracked (' . $item . ')']
-      ]
-    )
+  throw new Exception(
+    'item not being tracked (' . $item . ')'
   );
 
 http_response_code(200);
