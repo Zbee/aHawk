9cb0a89ff3502db36ef3e8d0e839f579422e1c00
Ethan Henderson <ethan@zbee.me>
2015-08-07 20:23:06 -0600

    Remove useless file

--- items/index.php
+++ items/index.php
@@ -11,7 +11,6 @@
     <link rel='stylesheet' href='/assets/css/style.css' />
     <script src='/assets/js/jquery.js'></script>
     <script src="/assets/js/highcharts.js"></script>
-    <script src="/assets/js/exporting.js"></script>
   </header>
 
   <body>
